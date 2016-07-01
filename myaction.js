51fc8d54a546f38c9e5b6d75cca00bce563c3306
JiaminZhu <Jiamin.Zhu@huawei.com>
2016-06-30 20:45:37 -0700

    upload examples

---
+++ myaction.js
@@ -0,0 +1,18 @@
+/**
+  * main() will be invoked when you Run This Action.
+  * 
+  * @param Whisk actions accept a single parameter,
+  *        which must be a JSON object.
+  *
+  * @return The return value must also be JSON.
+  *         It will be the output of this action.
+  *
+  */
+function main(params) {
+	var output = params.commits[0].message;
+	return { "text": output,
+	         "payload": output
+	        }};
+}
+
+/* Hint: To invoke this action, click Run This Action */
